d24a73da0fbad8660bea2825d3fcbbccf968bbe8
Max Wiseman <maxwise70@hotmail.co.uk>
2024-08-07 18:31:30 +0000

    chore: Update button.test.js to include DOM setup for testing

--- assets/tests/button.test.js
+++ assets/tests/button.test.js
@@ -7,8 +7,15 @@
 const buttonClick = require("/workspace/JestTesting/assets/tests/button.js");
 
 beforeAll(() => {
-    document.body.innerHTML = "<p id='par'></p>";
+    let fs = require("fs");
+    let fileContents = fs.readFileSync("index.html", "utf-8");
+    document.open()
+    document.write(fileContents);
+    document.close()
 });
+// The above code reads the contents of the index.html file and writes it to
+//the document object before running the tests. This is so the tests can access the DOM elements freshly
+//This is boilerplate code that is used to set up the DOM for testing
 
 describe("DOM tests", () => {
     test("Expects content to change", () => {
@@ -16,4 +23,13 @@ describe("DOM tests", () => {
         expect(document.getElementById("par")
             .innerHTML).toEqual("You Clicked");
     });
+    test('h1 should exist', () => {
+        expect(document.getElementsByTagName('h1').length).toBe(1);   
+    })
 });
+
+// The above code tests the buttonClick function in button.js. It uses Jest's expect
+// function to check if the innerHTML of the element with the id "par" changes to "You Clicked" after the
+// buttonClick function is called. The beforeAll function is used to read the contents of the index.html 
+// file, which contains the element with the id "par". The jsdom environment is used to simulate a browser
+// environment for testing DOM manipulation.
